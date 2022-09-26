3524a9bb52df3b4944cbeacb585af46004ea6dd9
kadjik <kadjik_gasparyan@mail.ru>
2022-09-26 16:56:46 +0300

    release2

--- pages/api/currency.tsx
+++ pages/api/currency.tsx
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from "next"
+
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     res.status(200).json({
       currency: 'RUB/USD',
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,16 +7,25 @@ export default function Home() {
   const [state, setState] = useState({})
 
   useEffect(() => {
+    async function myFetch() {
+      const data = await fetch(`/api/currency`)
+      let log = await data.json()
+      setState(log)
+    }
+    myFetch()
+  }, [])
+
+  useEffect(() => {
     setInterval(async () => {
-      const data = await fetch('http://localhost:3000/api/currency')
+      const data = await fetch(`/api/currency`)
       let log = await data.json()
       setState(log)
     }, 10000)
-  }, [state])
-
+  }, [])
+  
   return (
-    <div>
-      <p>
+    <div className="h-14 bg-gradient-to-r from-purple-500 to-pink-500">
+      <p className="text-center" >
         Курс {state.currency}: {state.rate}
       </p>
     </div>
